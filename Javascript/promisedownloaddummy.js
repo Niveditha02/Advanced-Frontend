4fa4505bb5d15f6e61bb7d9d5032425453bfff5e
Niveditha02 <niveditha.mpn@gmail.com>
2025-04-10 22:40:20 -0400

    my fourth  commit

--- Javascript/promisedownloaddummy.js
+++ Javascript/promisedownloaddummy.js
@@ -6,10 +6,28 @@ function download(url){
                   const content = "ABCDEF";
                   res(content);
             },5000);
-      })
+      });
 }
 
-download("www.xyz.com")
-.then(function fulfillhandler(value){
-      console.log("content downloaded is",value);//here using promises we are not giving access to te third paty function like as in example callbackdownload.js
-})
+// download("www.xyz.com")
+// .then(function fulfillhandler(value){
+//       console.log("content downloaded is",value);//here using promises we are not giving access to te third paty function like as in example callbackdownload.js
+// })
+
+//dot.then chaining is whatever the value of the first promise handler next is the value of the second promise handler
+x = download("www.xyz.com");
+x.
+then(
+      function fulfillhandler1(value){
+            console.log("content downloaded is",value);
+            return "New promise string";
+      },
+      function rejectionhandler1(value){
+            console.log("rejected with",value);
+      }
+)
+.then(
+      function newFullfillhandler(value){
+            console.log("value from chained then promise",value);
+      }
+)
---
+++ Javascript/writingdownloads.js
@@ -0,0 +1,50 @@
+//Tasks: (Dont use promise only use callbacks)
+//1. Write a function to download data from a url
+//2. write a function to save that downloaded data in a file and return the filename
+//3. write a function to upload the file written in previous step to a newurl
+function download(url,cd){
+      console.log("starting to download data from",url);
+      setTimeout(function down(){
+            console.log("Downloading completed");
+            const content = "ABCDEF";
+            cd(content);
+      },4000);
+}
+
+download("www.xyz.com",function exec(content){
+      console.log("The downloading from the url is completed with a content",content);
+})
+
+//2. writing a function to save that downloaded data in a file and return the filename
+function writeFilename(data,cd){
+      console.log("Started downloading after 5s",data);
+      setTimeout(function write(){
+            console.log("Completed writing the data in a file");
+            const filename = "file.txt";
+            cd(filename);
+      },500);
+
+}
+
+writeFilename("abcdef",function process(name){
+      console.log("file written with name",name);
+})
+
+//upload the filename
+function upload(url,file,cd){
+      console.log("started uploading",file,"on",url);
+      setTimeout(function up(){
+            console.log("uploading started");
+            const response = "Success";
+            cd(response);
+      },1000);
+}
+
+download("www.xyz.com",function processDownload(content){
+      writeFilename(content,function processwrite(file){
+            console.log("We have downloaded and written the file, now will upload");
+            upload("www.xyz.com",filename,function processUpload(response){
+                  console.log("We have uploaded with",response);
+            });
+      });
+});
